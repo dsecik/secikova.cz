c7a199019da15481816ceedfb9007654d9a67c14
Daniel Secik <dsecik@gmail.com>
2016-09-02 14:59:33 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jandourkova.cz
---
+++ index.php
@@ -0,0 +1,547 @@
+﻿<html>
+<head>
+    <meta name="description" content="VÝUKA, PŘEKLADY A TLUMOČENÍ"/>
+    <meta name="keywords" content="výuka, překlady, tlumočení, němčina, neměcký, jazyk "/>
+    <meta name="robots" content="all, follow"/>
+<title>Petra Jandourková</title>
+<meta charset="utf-8">
+<style type="text/css">
+body {
+    FONT-FAMILY                : Arial;
+
+}
+A
+{
+    COLOR                      : white;
+    FONT-FAMILY                : Arial;
+    FONT-SIZE                  : 12pt;
+    FONT-WEIGHT                : none;
+    TEXT-DECORATION            : none;
+}
+A:hover{
+    COLOR                      : f5891f;
+    FONT-FAMILY                : Arial;
+    FONT-SIZE                  : 12pt;
+    FONT-WEIGHT                : none;
+    TEXT-DECORATION            : none;
+}
+</style>
+
+
+</head>
+
+
+<body bgcolor="white" text="black" topmargin="0" leftmargin="0" rightmargin="0" cellpadding="0" cellspacing="0">
+
+
+<center>
+
+
+
+<table width="100%" height="350" cellpadding="0" cellspacing="0" border="0" bgcolor="092147">
+	<tr>
+		<td valign="center">
+			<center>
+				<font color="white"><font size="+5">Petra Jandourková</font></font>
+				<p>
+				<font color="white"><font size="+3">VÝUKA, PŘEKLADY A TLUMOČENÍ</font></font>
+				<p>
+				<font color="white"><font size="+2"><b>NĚMECKÝ JAZYK</b></font></font>
+				<p>
+
+			</center>
+		</td>
+	</tr>
+</table>
+
+
+<table width="100%" height="400" cellpadding="0" cellspacing="0" border="0" bgcolor="0f3062">
+	<tr>
+		<td valign="center">
+			<center>
+
+
+
+
+
+
+<table width="960" height="400" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="360" height="400">
+<img src="foto.png">
+		</td>
+		<td width="600" height="400">
+<font color="white">
+
+<b>Nabízím překlady a tlumočení z němčiny do češtiny i z češtiny do němčiny<br> 
+a výuku němčiny v Brně.</b> 
+<p>
+
+
+Vystudovala jsem překladatelství a učitelství němčiny na Masarykově univerzitě <br>
+a těmto profesím se věnuji od roku 2012. Zaměřuji se hlavně na překlady webových stránek, firemní korespondence a textů z oblasti IT, ekologie, knihovnictví a dalších. Záleží mi na kvalitě překladu a spolehlivém dodržování termínů.   
+<p>
+
+
+Ve výuce je kladen důraz na praktické využití jazyka a preference klienta. I z tohoto důvodu upřednostňuji individuální lekce a výuku malých skupin. Kromě obecného jazyka nabízím pro mírně a středně pokročilé také individuální či skupinovou výuku obchodní korespondence (psaní emailů, telefonování, motivační dopis atd.). 
+
+<font>
+		</td>
+	</tr>
+
+
+			</center>
+		</td>
+	</tr>
+</table>
+
+
+
+
+</table>
+
+<table width="960" height="300" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="460" height="">
+<font size="+2"><font color="0f3062"><b>VÝUKA</b></font>
+</font><p>
+- individuální, skupinová či firemní výuka<br>
+- obecná němčina pro začátečníky i pokročilé<br>
+
+- obchodní němčina (telefonování, emaily, pracovní pohovor, životopis, motivační dopis)
+<p>
+
+
+Výuka zpravidla probíhá v učebně na ulici Lidická v Brně, mohu ale samozřejmě přijet i přímo k Vám. 
+		</td>
+		<td width="50" height="">
+
+		</td>
+		<td width="450" height="" bgcolor="">
+
+
+
+
+<table width="450" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="220" height="50">
+			<center><b>CENÍK</b></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white"><b>60 minut</b></font></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white"><b>90 minut</b></font></center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center>individuální lekce</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>400</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>550</b> Kč</center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center>skupiny 2 a více osob</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>550</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>700</b> Kč</center>
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+
+<p>
+V případě dojíždění za klientem po Brně v rámci zón 100+101 je navíc účtován poplatek 70 Kč.
+		</td>
+	</tr>
+</table>
+
+<p>
+
+
+<table width="960" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td>
+
+		</td>
+	</tr>
+</table>
+
+
+<p>
+
+<table width="960" height="300" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="460" height="">
+<font size="+2"><font color="0f3062"><b>PŘEKLADY</b></font>
+</font><p>
+- obecné i odborné překlady z němčiny do češtiny a z češtiny do 
+němčiny<br>
+
+- možnost expresních termínů<br>
+
+- překlady webových stránek, prezentací, výročních zpráv, textů z oblasti IT, 
+ekologie, pedagogiky, knihovnictví, cestovního ruchu a dalších
+<p>
+
+
+Pro zachování grafické podoby je nutné zaslat soubor <br>v editovatelném formátu. 
+		</td>
+		<td width="50" height="">
+
+		</td>
+		<td width="450" height="250" bgcolor="">
+
+
+
+
+<table width="450" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="220" height="50">
+			<center><b>CENÍK</b></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white">z němčiny do češtiny</font></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white">z češtiny do němčiny</font></center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center><b>obecné texty</b><br>
+				(webové stránky, obchodní korespondence..)
+			</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>290</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>320</b> Kč</center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center><b>odborné texty</b><br>
+				(manuály, smlouvy..)
+			</center>
+		</td>
+		<td width="120" height="50">
+			<center>od <b>350</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center>od <b>450</b> Kč</center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center><b>expresní překlady</b><br>
+				do 24 hodin
+			</center>
+		</td>
+		<td width="120" height="50">
+			<center>dle domluvy</center>
+		</td>
+		<td width="120" height="50">
+			<center>dle domluvy</center>
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+<p>
+Ceny jsou uvedené za normostranu výchozího textu, tzn. 1800 znaků včetně mezer, a závisí na terminologické náročnosti textu a termínu dodání překladu. 
+
+Minimální cena zakázky je 200 Kč.
+		</td>
+	</tr>
+</table>
+
+<p>
+
+<table width="960" height="300" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="460" height="">
+<font size="+2"><font color="0f3062"><b>TLUMOČENÍ</b></font>
+</font><p>
+- doprovodné a konsekutivní tlumočení z němčiny do češtiny <br>a z češtiny 
+do němčiny<br>
+
+- obchodní jednání, porady, prezentace, společenské akce
+<p>
+<b>Doprovodné tlomočení</b> se využívá především při obchodních cestách a společenských akcích. Tlumočník usnadňuje orientaci zákazníka v cizojazyčném prostředí. 
+<p>
+
+
+Při <b>konsekutivním tlumočení</b> hovoří řečník v kratších úsecích <br> a dělá ve svém projevu pauzy, v nichž tlumočník převede vyslovené myšlenky do cílového jazyka. 
+		</td>
+		<td width="50" height="">
+
+		</td>
+		<td width="450" height="250" bgcolor="">
+
+
+
+
+<table width="450" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="220" height="50">
+			<center><b>CENÍK</b></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white"><b>půl dne</b></br>
+				(cca 4 hodiny)</font></center>
+		</td>
+		<td width="120" height="50" bgcolor="0f3062">
+			<center><font color="white"><b>celý den</b></br>
+				(cca 8 hodin)</font></center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center>doprovodné tlumočení</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>3.500</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>6.000</b> Kč</center>
+		</td>
+	</tr>
+	<tr>
+		<td width="220" height="10">
+		</td>
+	</tr>
+	<tr bgcolor="e8e7e7">
+		<td width="220" height="50">
+			<center>konsekutivní tlumočení</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>4.500</b> Kč</center>
+		</td>
+		<td width="120" height="50">
+			<center><b>7.000</b> Kč</center>
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+<p>
+Ceny jsou konečné, nejsem plátcem DPH.  
+		</td>
+	</tr>
+</table>
+
+<p>
+
+<table width="960" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+<font size="+2"><font color="0f3062"><b>REFERENCE</b></font>
+</font><p>
+<b>Výuka</b>
+	</tr>
+	<tr>
+		<td width="470" height="">
+
+<i>I would absolutely recommend Petra Jandourkova as a German tutor. She tutored my 12 year old son in German for 8 months and we were extremely satisfied. Petra does a phenomenal job in preparing for each lesson; she always plans interesting activities that kept my son very engaged in learning. He actually looked forward to his lesson every week! Petra is very well organized and highly skilled in teaching individuals of all ages.
+	
+	<p>						<b>Tyler Golden</b></i>
+		</td>
+		<td width="20" height="">
+
+		</td>
+
+		<td valign="top" width="470" height="">
+<i>Po tolika letech předstírání učení se němčiny jsem konečně pochopila, že je to opravdu jazyk určený především ke komunikaci mezi lidmi. Jen díky Petře jsem byla schopná projít několika pohovory v němčině – jak po telefonu, tak i osobními, a získat <br>v Německu práci. Před půl rokem bych tomu rozhodně nevěřila.	
+	<p>						<b>Anna Kubalíková</b></i>
+		</td>
+	</tr>
+</table>
+
+<p>
+
+
+<table width="960" height="" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+<b>Překlady</b>
+<p>
+Red Hat Czech, s.r.o. <br>
+Moravská zemská knihovna v Brně<br>
+Förderverein Natura Miriquidica e.V.<br>
+Checkstone Survey Technologies GmbH<br>
+		</td>
+	</tr>
+</table>
+
+
+
+
+
+
+
+<p>
+
+
+<table width="100%" height="400" cellpadding="0" cellspacing="0" border="0" bgcolor="0f3062">
+	<tr>
+		<td>
+<center><br><br>
+<style>
+	.mf input[type="text"], .mf textarea {
+		padding: 5px;
+		border-radius: 0px;	/* Zaoblení rohů */
+		border: 1px solid silver; /* ohraničení textového pole */
+	}
+	
+	/* Styly pro tlačítko */
+	.mf input[type="submit"] {
+		border: 1px solid silver;
+		background: #0077b5;
+		color: white;
+		padding: 10px 20px 10px 20px;
+		transition: background 0.5s;
+	}
+	.mf input[type="submit"]:hover {
+		background: green;
+	}
+</style>
+
+<form action="form_success.php" method="post" class="mf">
+	<table border="0">
+		<tr>
+			<td align="right"><font color="white">Jméno:</font></td>
+			<td><input type="text" name="name" id="mf-name"></td>
+		</tr>
+		<tr>
+			<td align="right"><font color="white">E-mail:</font></td>
+			<td><input type="text" name="email" id="mf-email"></td>
+		</tr>
+		<tr>
+			<td align="right" valign="top"><font color="white">Zpráva:</font></td>
+			<td><textarea name="message" rows="4" cols="60" id="mf-message"></textarea></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><input type="submit" value="Odeslat"></td>
+		</tr>
+	</table>
+</form>
+
+<script src="https://code.jquery.com/jquery-3.1.0.slim.min.js" integrity="sha256-cRpWjoSOw5KcyIOaZNo4i6fZ9tKPhYYb6i5T9RSVJG8="crossorigin="anonymous"></script>
+
+<script>
+	jQuery(document).ready(function($) {
+		$(".mf").submit(function() {
+			if ($("#mf-name").val() == '') {
+				alert('Vyplňte prosím jméno!');
+				$("#mf-name").focus();
+				return false;
+			}
+			if ($("#mf-email").val() == '') {
+				alert('Vyplňte prosím email!');
+				$("#mf-email").focus();
+				return false;
+			}
+			if ($("#mf-message").val() == '') {
+				alert('Vyplňte prosím zprávu!');
+				$("#mf-message").focus();
+				return false;
+			}
+		});
+	});
+</script>
+</center>
+	</td>
+	</tr>
+	<tr>
+		<td valign="center">
+			<center>
+
+
+<table width="960" height="400" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td width="550" height="">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2606.8461860825582!2d16.605046215767647!3d49.20347738430032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4712945ccdfa4a95%3A0x6237635be1c0394!2zTGlkaWNrw6EgMTg3NS80MCwgNjAyIDAwIEJybm8tc3TFmWVkLcSMZXJuw6EgUG9sZQ!5e0!3m2!1scs!2scz!4v1470831904981" width="550" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+		</td>
+		<td width="50" height="">
+		</td>
+		<td width="360" height="">
+<font color="white">
+
+<font size="+1"><b>KONTAKT</b></font>
+<p>
+
+
+Mgr. et Mgr. Petra Jandourková<br>
+<p>
+
+
+Lidická 40<br>
+
+Brno - střed<br>
+<p>
+
+
+jandourkova.petra@email.cz<br>
+
++420774169573<br>
+<p>
+<a href="https://www.linkedin.com/in/petra-jandourkova-39a886104" target="_blank"><img src="linkedin.png"></a>
+
+<br><br>
+
+
+		</td>
+	</tr>
+
+			</center>
+		</td>
+	</tr>
+</table>
+
+		</td>
+	</tr>
+</table>
